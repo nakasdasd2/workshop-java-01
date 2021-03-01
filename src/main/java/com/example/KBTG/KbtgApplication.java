cf6a22f63d43816b783179b710d77c107dae11dc
parunyu.d <parunyu.d@kbtg.tech>
2021-03-01 11:15:10 +0700

    Workshop 1

---
+++ src/main/java/com/example/KBTG/KbtgApplication.java
@@ -0,0 +1,13 @@
+package com.example.KBTG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbtgApplication.class, args);
+	}
+
+}
